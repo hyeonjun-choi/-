ed34823da0c30153d59fa1855fda9ae2d6818c43
hyeonjun-choi <garada123@naver.com>
2021-07-13 18:16:02 +0900

    20210713

--- practice1/src/classpart/Man.java
+++ practice1/src/classpart/Man.java
@@ -2,16 +2,36 @@ package classpart;
 
 public class Man {
 
-	int age;
-	String man_Name;
-	boolean isMarried;
-	int child;
-	
-	public String getMan_Name( ) {
-		return man_Name;
-	}
+	int age;			//] 클래스 내의 출력하고 싶은 세부 항목을 올바른 출력값과 함께 작성
+	String manName;		//]
+	boolean isMarried;	//]
+	int child;			//]
 	
 	public void showManInfo( ) {
-		System.out.println(man_Name + " , " + age + " , " + isMarried + " , " + child);
+		System.out.println(manName + " , " + age + " , " + isMarried + " , " + child);
+	}	//위의 항목들을 포함한 메서드를 추가
+	
+	public String getManName( ) {
+		return manName;
+	}	//이름값을 반환하는 메서드
+	
+	public void setManName(String name) {
+		manName = name;
+	}	// 매개변수를 멤버변수에 대입하여 멤버변수를 통해 클래스를 구현하게 하는 메서드
+	
+	public static void main(String[] args) {
+		int age = 40;
+		
+		Man ManJ = new Man();
+		ManJ.manName = "James";
+		
+		boolean isMarried = true;	// boolean의 결과는 참, 거짓 말고 없다.
+		
+		int child = 3;
+		
+		System.out.println(age);
+		System.out.println(ManJ.manName);
+		System.out.println(isMarried);
+		System.out.println(child);
 	}
 }
---
+++ practice1/src/classpart/Order.java
@@ -0,0 +1,19 @@
+package classpart;
+
+public class Order {
+
+	int orderNum;
+	String orderID;
+	boolean Date;
+	String orderName;
+	String orderProductNum;
+	String address;
+	
+	public String getOrderName() {
+		return orderName;
+	}
+	
+	public static void main(String[] args) {
+		Order orderHong = new Order();
+	}
+}
